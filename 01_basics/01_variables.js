a7884e404630e2bb1fe1d573d342e3dfea7e25f2
sarthakbhai395 <sarthakbhatnagar2005@gmail.com>
2026-01-29 00:50:33 +0530

    practicing variables and datatypes in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const Name ="sarthak";
+let Email="s@gmail.com";
+var Password=12345;
+StateName="agra";
+let address;
+
+
+// Name="devansh";
+Email="d@gmail.com";
+Password=22222;
+StateName="noida";
+
+
+
+console.table([Name,Email,Password,StateName,address]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+let age =20;
+let Name="Sarthak";
+let isloggesdIn=true;
+let state=null;
+let city=undefined;
+
+
+/*
+     number =>2*10^53;
+     string = "";
+     boolean= true/false;
+     Bigint= for lager value or number;
+     null= standalone values;
+     symbol=unique;
+    undefined = no value assigned ;
+    
+     object =collection of related data;
+
+*/
+
+console.table([typeof age, typeof Name, typeof isloggesdIn, typeof state, typeof city])
