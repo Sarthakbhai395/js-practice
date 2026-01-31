2ee54049366bf42f0501c9d3097500b27b48d81f
sarthakbhai395 <sarthakbhatnagar2005@gmail.com>
2026-02-01 01:37:14 +0530

    Add string methods and operations practice

---
+++ 01_basics/11_strings.js
@@ -0,0 +1,38 @@
+const name="sarthak";
+let age =20
+// console.log(name + age +"i live in agra ");
+ console.log(`my name is ${name} and i am ${age}years  old developer `);
+ const gamechanger=new String ("Sarthak bhatnagar");
+
+ console.log(gamechanger[0]);
+
+ console.log(gamechanger.toUpperCase());
+
+  console.log(gamechanger.length);
+
+  console.log(gamechanger.split(" "));
+  
+  console.log(gamechanger.includes("an awesome boy "));
+
+  console.log(gamechanger.includes("Sarthak"));
+  
+  
+  console.log(gamechanger.indexOf(5));
+
+  console.log(gamechanger.charAt(2));
+  const anotherString=gamechanger.slice('-7',4);
+console.log(anotherString);
+  
+ 
+const newString = "   sarthak    ";
+console.log(newString);
+console.log(newString.trim());
+
+const url="https://www.sarthak.com/about?name=sarthak&age=20";
+
+console.log(url.replace("=20","no"));
+
+
+// const anotherString=gamechanger.slice('-7',4);
+// console.log(anotherString);
+ 
