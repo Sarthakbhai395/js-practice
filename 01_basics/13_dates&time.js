eaed5ccbc800418b70d2f48564b3e9d323d3404a
sarthakbhai395 <sarthakbhatnagar2005@gmail.com>
2026-02-07 01:29:20 +0530

    Enhance date and time examples with additional methods

--- 01_basics/13_dates&time.js
+++ 01_basics/13_dates&time.js
@@ -2,15 +2,15 @@ let MyDate = new Date();
 // console.log(MyDate.toString());
 
 // console.log(MyDate.toLocaleString());
-// console.log(MyDate.toDateString());
-// console.log(MyDate.toJSON());
+//console.log(MyDate.toDateString());
+//console.log(MyDate.toJSON());
 // console.log(typeof MyDate);
 
 let newDate = new Date(2026,0,4,1,55);
 // console.log(newDate.toLocaleString());
 
 let myTimeStamp = Date.now();
-// console.log(myTimeStamp);
+ //console.log(myTimeStamp);
 
 
 // console.log(newDate.getTime());
