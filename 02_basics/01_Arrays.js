0829e5805c907bacf7b56d39aa16ef8cb37740a5
sarthakbhai395 <sarthakbhatnagar2005@gmail.com>
2026-02-07 01:29:31 +0530

    Add arrays practice in new basics section

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,46 @@
+let MyArray =[1,2,3,4,5];
+let Myheros = ['shaktiman, naagraj, doga, krish'];
+
+// console.log([heros]);
+// console.log([2]);
+
+// const myArr2 = new Array(1, 5, 3, 4)
+// console.log(MyArray[1]);
+
+// console.log(myArr2[2]);
+
+// console.log(heros.length);
+
+
+// MyArray.push(6);
+// console.log(MyArray);
+
+
+// Myheros.push('superman');
+// console.log(Myheros);
+
+// MyArray.pop();
+// console.log(MyArray);
+
+
+// MyArray.unshift(9);
+// console.log(MyArray);
+
+// console.log();
+
+// console.log(MyArray.indexOf(9));
+// Myheros.shift();
+// console.log(Myheros);
+
+const newArray = MyArray.join("-");
+console.log(MyArray);
+console.log(newArray);
+
+// +++++++++++++   Slice & Splice ++++++++++++++++++++++++++++
+
+const new1 = MyArray.slice(1,4);
+
+
+console.log(MyArray);
+console.log(new1);
+
