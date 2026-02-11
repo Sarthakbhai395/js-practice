fb6099daa8b6ee2abcb2e996fa7b41fefcbd4a36
sarthakbhai395 <sarthakbhatnagar2005@gmail.com>
2026-02-12 01:35:53 +0530

    Add JavaScript functions practice and examples

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,27 @@
+// function MyName(){
+
+//     console.log("s");
+// }
+
+// MyName();
+
+// function AddTwoNumbers(number1 , number2){
+// // console.log(number1 + number2);
+// let Result = number1+ number2;
+// return Result 
+// }
+// // console.log(result);
+// const Result  = AddTwoNumbers(3,5);
+// console.log('Result:' , Result);
+
+function LOginUserMessage (username ="sarthak"){
+    if (username==="sarthak"){
+        console.log("welcome user ");
+        return 
+        
+
+    }
+    // return `${username} logged in successfully `;
+}
+ console.log(LOginUserMessage("sarthak"));
+ 
