b71de856047dffd1290b8d243f1a871b48659c0b
sarthakbhai395 <sarthakbhatnagar2005@gmail.com>
2026-02-13 01:45:33 +0530

    Add JavaScript functions practice and examples

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,14 +14,60 @@
 // const Result  = AddTwoNumbers(3,5);
 // console.log('Result:' , Result);
 
-function LOginUserMessage (username ="sarthak"){
-    if (username==="sarthak"){
-        console.log("welcome user ");
-        return 
+// function LOginUserMessage (username ="shivam"){
+//     if (username==="sarthak"){
+//         console.log("welcome user ");
+//     }
+//         else(username!=="sarthak")
+//             console.log("user not found");
+            
+        
+//         return 
         
 
-    }
-    // return `${username} logged in successfully `;
-}
- console.log(LOginUserMessage("sarthak"));
+//     }
+//     // return `${username} logged in successfully `;
+
+//  console.log(LOginUserMessage("sarthak"));
  
+
+
+
+// function addtwonumbers(number1,number2){
+//     let result = number1+ number2;
+
+//    return result
+// }
+
+// const result = addtwonumbers(3,5);
+// console.log('result:', result);
+
+// function calculation(val1, val2 , ...num1){
+//     return num1
+// }
+// console.log(calculation(100,400,500,2000));
+
+// const user = {
+//     username :"sarthak",
+//     price :199
+// }
+
+// function handleobject(anyobject){
+//     console.log(`the name of the user is ${anyobject.username} and the price is ${anyobject.price}`);
+    
+// }
+
+// handleobject(user)
+
+
+function calculateproducts ( val1, val2,...num1) {
+    let result = val1 + val2
+return 
+}
+ console.log(calculateproducts(200,400,300,100));
+//  console.log(typeof calculateproducts);
+console.log('result:',200+400 );
+
+
+ 
+
